d91310cf0b2bf7c6b0cd9055d9284bed3c098703
Mohamed Mehdi Gharam <ge47wog@mytum.de>
2024-08-28 12:28:10 +0200

    fixed hello_world example

--- examples/hello_world.rs
+++ examples/hello_world.rs
@@ -14,23 +14,24 @@ pub fn main() -> Result<(), Box<dyn Error>> {
     };
 
     let mut nvme = vroom::init(&pci_addr)?;
+    let ns_id = 1;
     //nvme.write_copied("hello world".as_bytes(), 0)?;
-    nvme.zone_action(1, 0, false, vroom::ZnsZsa::ResetZone)?;
+    nvme.zone_action(ns_id, 0, false, vroom::ZnsZsa::ResetZone)?;
 
-    nvme.append_io_copied(1, 0, "hello world".as_bytes())?;
-    nvme.append_io_copied(1, 0, "hello world".as_bytes())?;
+    nvme.append_io_copied(ns_id, 0, "hello world".as_bytes())?;
+    nvme.append_io_copied(ns_id, 0, "hello world".as_bytes())?;
 
     let mut dest = [0u8; 12];
-    nvme.read_copied(&mut dest, 0)?;
+    nvme.read_copied(ns_id, &mut dest, 0)?;
 
     println!("{}", std::str::from_utf8(&dest)?);
 
-    nvme.read_copied(&mut dest, 2)?;
+    nvme.read_copied(ns_id, &mut dest, 2)?;
 
     println!("{}", std::str::from_utf8(&dest)?);
 
 
-    nvme.zone_action(1, 0, false, vroom::ZnsZsa::CloseZone)?;
+    nvme.zone_action(ns_id, 0, false, vroom::ZnsZsa::CloseZone)?;
 
     Ok(())
 }
